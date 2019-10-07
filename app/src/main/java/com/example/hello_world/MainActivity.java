c71c34e8598bd75dbc5a5fc6a14a8487d0810f4c
jiim92 <michael.strasser@brg-judenburg.ac.at>
2019-10-07 15:05:47 +0200

    added first comment

--- app/src/main/java/com/example/hello_world/MainActivity.java
+++ app/src/main/java/com/example/hello_world/MainActivity.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    // Meine erste App - Das ist die Java-Datei zur Hauptoberfläche
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
